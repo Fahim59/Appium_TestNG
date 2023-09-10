be318309a26be824aaedec1b8898502cfe89aea1
fahim59 <mrahaman59@yahoo.com>
2023-09-10 16:52:59 +0600

    test package added

---
+++ src/test/java/tests/HomeTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import base.BaseClass;
+import org.testng.annotations.*;
+
+public class HomeTest extends BaseClass {
+
+    @Test
+    public void home_test() {
+        System.out.println("Test");
+    }
+}
