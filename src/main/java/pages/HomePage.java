8e1be770e2f981423f385efbba12775614ca7ff4
fahim59 <mrahaman59@yahoo.com>
2023-09-10 16:36:05 +0600

    pages package added

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,10 @@
+package pages;
+
+import base.BaseClass;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+
+public class HomePage extends BaseClass {
+
+    @AndroidFindBy(xpath = "") private MobileElement x;
+}
