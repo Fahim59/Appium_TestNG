a1010ed0d021f808f95e0aa8f0fe0ba54702299f
fahim59 <mrahaman59@yahoo.com>
2023-09-13 15:49:28 +0600

    validate product details by xml file

--- src/main/java/pages/ProductPage.java
+++ src/main/java/pages/ProductPage.java
@@ -1,6 +1,36 @@
 package pages;
 
 import base.BaseClass;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
 
 public class ProductPage extends BaseClass {
+    @AndroidFindBy(uiAutomator = "text(\"Sauce Labs Onesie\")") private MobileElement productTitle;
+    @AndroidFindBy(xpath = "//*[@content-desc = 'product price'][1]") private MobileElement productPrice;
+    @AndroidFindBy(xpath = "//*[@content-desc = 'product description'][1]") private MobileElement productDescription;
+
+    @AndroidFindBy(xpath = "//*[@content-desc = 'Add To Cart button'][1]") private MobileElement addToCartBtn;
+    @AndroidFindBy(xpath = "//*[@content-desc = 'counter plus button'][1]") private MobileElement plusBtn;
+
+    public String getProductTitle(){
+        return getText(productTitle);
+    }
+
+    public String getProductPrice(){
+        return getText(productPrice);
+    }
+
+    public String getProductDescription(){
+        return getText(productDescription);
+    }
+
+    public ProductPage clickAddToCartButton(){
+        click_Element(addToCartBtn);
+        return this;
+    }
+
+    public ProductPage clickPlusButton(){
+        click_Element(plusBtn);
+        return this;
+    }
 }
--- src/test/java/tests/ProductTest.java
+++ src/test/java/tests/ProductTest.java
@@ -1,14 +1,26 @@
 package tests;
 
 import base.BaseClass;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.ProductPage;
+import utils.ConfigLoader;
 
 public class ProductTest extends BaseClass {
     private ProductPage productPage;
     private HomePage homePage;
+
+    String productName;
+    String productPrice;
+
+    @BeforeClass
+    public void beforeClass() throws Exception {
+        productName = new ConfigLoader().parseStringXML().get("product_name");
+        productPrice = new ConfigLoader().parseStringXML().get("product_price");
+    }
 
     @BeforeMethod
     public void beforeMethod() {
@@ -21,5 +33,11 @@ public class ProductTest extends BaseClass {
         scroll_down_text_findElement("Sauce Labs Onesie");
 
         homePage.clickProductTitle();
+
+        String title = productPage.getProductTitle();
+        Assert.assertEquals(title, productName);
+
+        String price = productPage.getProductPrice();
+        Assert.assertEquals(price, productPrice);
     }
 }
