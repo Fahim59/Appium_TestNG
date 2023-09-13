c699426a9db9aee6113b107f932ba9e43491df7d
fahim59 <mrahaman59@yahoo.com>
2023-09-13 13:53:34 +0600

    home page added

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,5 +6,53 @@ import io.appium.java_client.pagefactory.AndroidFindBy;
 
 public class HomePage extends BaseClass {
 
-    @AndroidFindBy(xpath = "") private MobileElement x;
+    @AndroidFindBy(xpath = "//*[@content-desc='open menu']") private MobileElement menuBtn;
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text = 'Log In']") private MobileElement loginMenuBtn;
+    @AndroidFindBy(xpath = "//android.widget.TextView[@text = 'Log Out']") private MobileElement logoutMenuBtn;
+
+    @AndroidFindBy(id = "android:id/button1") private MobileElement logoutConfirmBtn;
+    @AndroidFindBy(id = "android:id/button1") private MobileElement okBtn;
+
+    @AndroidFindBy(xpath = "//*[@content-desc='cart badge']") private MobileElement cartBtn;
+
+    @AndroidFindBy(uiAutomator = "text(\"Sauce Labs Onesie\")") private MobileElement productTitle;
+
+    public HomePage clickMenuButton(){
+        click_Element(menuBtn);
+        return this;
+    }
+
+    public HomePage clickLoginMenuButton(){
+        click_Element(loginMenuBtn);
+        return this;
+    }
+
+    public HomePage clickLogoutMenuButton(){
+        click_Element(logoutMenuBtn);
+        return this;
+    }
+
+    public HomePage clickLogoutConfirmButton(){
+        click_Element(logoutConfirmBtn);
+        return this;
+    }
+
+    public HomePage clickOkButton(){
+        click_Element(okBtn);
+        return this;
+    }
+
+    public HomePage logout(){
+        return clickMenuButton().clickLogoutMenuButton().clickLogoutConfirmButton().clickOkButton();
+    }
+
+    public HomePage clickCartButton(){
+        click_Element(cartBtn);
+        return this;
+    }
+
+    public HomePage clickSLBTitle(){
+        click_Element(productTitle);
+        return this;
+    }
 }
