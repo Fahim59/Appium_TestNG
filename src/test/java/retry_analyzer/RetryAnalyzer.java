223137c70072145246d866303492d6f2a7016758
fahim59 <mrahaman59@yahoo.com>
2023-09-14 11:17:39 +0600

    retry analyzer added

---
+++ src/test/java/retry_analyzer/RetryAnalyzer.java
@@ -0,0 +1,20 @@
+package retry_analyzer;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+
+    int counter = 0;
+    int limit = 2;
+
+    @Override
+    public boolean retry(ITestResult iTestResult) {
+
+        if(counter < limit){
+            counter++;
+            return true;
+        }
+        return false;
+    }
+}
